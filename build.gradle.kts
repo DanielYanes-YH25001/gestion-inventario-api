1e1122b7d223e1b70b9ad703ff5805edc3f8f9fd
Daniel Yanes <yh25001@ues.edu.sv>
2026-09-08 16:28:09 -0600

    chore: agregar estructura inicial de archivos
    - Agregar archivos iniciales de configuración para Gradle/Kotlin
    - Agregar archivo .gitignore

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("java")
+}
+
+group = "ues.ids.poo"
+version = "0.1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
